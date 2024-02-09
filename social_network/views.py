09ed0fadb492432790cc0dbfb44830a985248f4d
Mykola <nickkozlov90@gmail.com>
2024-02-09 23:47:10 +0200

    Add user's search by first and last name

--- social_network/views.py
+++ social_network/views.py
@@ -21,3 +21,21 @@ class UserViewSet(
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsOwnerOrIfAuthenticatedReadOnly,)
     queryset = User.objects.all()
+
+    def get_queryset(self):
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        queryset = self.queryset
+
+        if first_name:
+            queryset = queryset.filter(
+                first_name__icontains=first_name
+            )
+
+        if last_name:
+            queryset = queryset.filter(
+                last_name__icontains=last_name
+            )
+
+        return queryset
